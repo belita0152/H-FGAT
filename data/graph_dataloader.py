6d013b24863619f379361549d9f004e1797ec90c
belita0152 <108735429+belita0152@users.noreply.github.com>
2025-12-12 20:36:10 +0900

    Update graph_dataloader.py

--- data/graph_dataloader.py
+++ data/graph_dataloader.py
@@ -1,11 +1,12 @@
+import os
 import torch
-import dgl
-from dgl.data import DGLDataset
-from dgl.dataloading import GraphDataLoader
+from torch.utils.data import Dataset
+from torch_geometric.loader import DataLoader
+from torch_geometric.data import Data
 
 device = torch.device('cuda:0')
 
-class GraphDataset(DGLDataset):
+class GraphDataset(Dataset):
     """
         edge.py에서 전처리되어 저장된 .pt 파일(x, y, edge)을 로드하여 제공
     """
@@ -13,49 +14,46 @@ class GraphDataset(DGLDataset):
     def __init__(self, saved_file_path):
         print(f"Loading {saved_file_path}...")
         data = torch.load(saved_file_path, weights_only=False)
-        dataset_name = os.path.splitext(os.path.basename(saved_file_path))[0]
 
         self.x = torch.tensor(data['x']).float()  # Raw EEG
         self.y = torch.tensor(data['y']).long()
-        self.edge = data['edge']  # Adj matrix (PLV)
+        self.edge = torch.tensor(data['edge']).float()  # Adj matrix (PLV)
 
-        super().__init__(name=dataset_name)
+        self.graph_list = self.process()
 
     def process(self):
-        graphs = []
+        graph_list = []
         n_samples, n_nodes = self.x.shape[0], self.x.shape[1]
 
         for i in range(n_samples):
-            node_features = self.x[i]  # (30, 384)
+            x_feat = self.x[i]  # (30, 384)
 
             adj = self.edge[i]  # (30, 30)
 
             adj.fill_diagonal_(0)  # self loop 제거
 
-            src, dst = torch.nonzero(adj, as_tuple=True)  # True인 인덱스 추출
-            g = dgl.graph((src, dst), num_nodes=n_nodes)
+            edge_index = torch.nonzero(adj, as_tuple=True) # True인 인덱스 추출
 
-            g.ndata['feat'] = node_features.float()
+            row, col = edge_index
+            edge_attr = adj[row, col]
 
-            weights = adj[src, dst]
-            g.edata['w'] = weights
+            graph = Data(x=x_feat, edge_index=edge_index, edge_attr=edge_attr, y=self.y[i])
 
-            graphs.append(g)
+            graph_list.append(graph)
 
-        self.graphs = graphs
-        self.graph_labels = self.y
+        return graph_list
 
     def __getitem__(self, item):
-        return self.graphs[item], self.graph_labels[item]
+        return self.graph_list[item]
 
     def __len__(self):
-        return len(self.graphs)
+        return len(self.graph_list)
 
 
 def get_dataloaders(train_path, test_path, batch_size):
     train_dataset, test_dataset = GraphDataset(train_path), GraphDataset(test_path)
-    train_dataloader,  test_dataloader = (GraphDataLoader(train_dataset, batch_size=batch_size, shuffle=True),
-                                           GraphDataLoader(test_dataset, batch_size=batch_size, shuffle=False))
+    train_dataloader,  test_dataloader = (DataLoader(train_dataset, batch_size=batch_size, shuffle=True, drop_last=True),
+                                           DataLoader(test_dataset, batch_size=batch_size, shuffle=False, drop_last=True))
 
     return train_dataloader, test_dataloader
 
