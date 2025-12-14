7774feb50fce86bfbc8cd0164f081f5b3abd3710
belita0152 <108735429+belita0152@users.noreply.github.com>
2025-12-14 20:32:00 +0900

    Implement sliding window and new dataset classes
    Added SlidingWindowDataset class for data preprocessing and modified AttentionDataset and DrowsinessDataset to use long tensors for labels. Introduced MotorImageryDataset class for loading motor imagery data.

--- data/dataloader.py
+++ data/dataloader.py
@@ -9,17 +9,55 @@ from torch.utils.data import Dataset, DataLoader
 
 
 # -----------------------------------------------------------------------------
+# Sliding Window
+# -----------------------------------------------------------------------------
+
+class SlidingWindowDataset(Dataset):
+    def __init__(self, window_size: int, stride: int):
+        self.window_size = window_size
+        self.stride = stride
+
+    def __call__(self, x, y):
+        if x.ndim == 2:
+            x = x[np.newaxis, ...]  # (1, channels, time)으로 통일
+            y = np.array([y])
+
+        n_trials, n_channels, n_time = x.shape
+
+        total_x, total_y = [], []
+
+        for i in range(n_trials):
+            trial_data = x[i]  # (Channels, Time)
+            trial_label = y[i]  # Scalar
+
+            start = 0
+            while start + self.window_size <= n_time:
+                segment = trial_data[:, start: start + self.window_size]  # (Channels, Window)
+
+                total_x.append(segment)
+                total_y.append(trial_label)
+
+                start += self.stride
+
+        return np.stack(total_x), np.array(total_y)
+
+# -----------------------------------------------------------------------------
 # We only use dataset with "_2" suffix, recordings of EEG during the mental arithmetic task.
 # -----------------------------------------------------------------------------
 
 class AttentionDataset(Dataset):
-    def __init__(self, path):
+    def __init__(self,
+                 base_path='/data/attention_npz'
+                 ):
         super().__init__()
-        self.x, self.y = self.get_data(base_path=path)
-        self.ch_names = ['Fp1' 'AFF5h' 'AFz' 'F1' 'FC5' 'FC1' 'T7' 'C3' 'Cz' 'CP5' 'CP1' 'P7' 'P3'
-                         'Pz' 'POz' 'O1' 'Fp2' 'AFF6h' 'F2' 'FC2' 'FC6' 'C4' 'T8' 'CP2' 'CP6' 'P4'
-                         'P8' 'O2' 'HEOG' 'VEOG']
+        self.x, y = self.get_data(base_path=base_path)
+        self.y = np.where(y > 1, 1, 0)
+
+        self.ch_names = ['Fp1', 'AFF5h', 'AFz', 'F1', 'FC5', 'FC1', 'T7', 'C3', 'Cz', 'CP5', 'CP1', 'P7', 'P3',
+                         'Pz', 'POz', 'O1', 'Fp2', 'AFF6h', 'F2', 'FC2', 'FC6', 'C4', 'T8', 'CP2', 'CP6', 'P4',
+                         'P8', 'O2', 'HEOG', 'VEOG']
         self.sfreq = 200
+
 
     def __getitem__(self, item):
         return torch.tensor(self.x[item]).float(), torch.tensor(self.y[item]).long()
@@ -59,9 +97,15 @@ class MentalArtihmeticDataset(Dataset):
         self.second = 10
         self.sfreq = 500
 
-        self.x, self.y = self.parser(self.data_path, self.labels)
+        self.window_size = 1000
+        self.stride = 500  # 50% overlap
 
-    def parser(self, data_path, labels):
+        raw_x, raw_y = self.load_raw_data(self.data_path, self.labels)
+
+        self.slider = SlidingWindowDataset(self.window_size, self.stride)
+        self.x, self.y = self.slider(raw_x, raw_y)
+
+    def load_raw_data(self, data_path, labels):
         total_x, total_y = [], []
         for i, x_path in enumerate(data_path):
             x = mne.io.read_raw_edf(x_path, preload=False)
@@ -89,12 +133,12 @@ class MentalArtihmeticDataset(Dataset):
 
     def __getitem__(self, item):
         x = torch.tensor(self.x[item], dtype=torch.float)
-        y = torch.tensor(self.y[item], dtype=torch.float)
+        y = torch.tensor(self.y[item], dtype=torch.long)
 
         return x, y
 
 
-class Drowsiness(Dataset):
+class DrowsinessDataset(Dataset):
     def __init__(
             self,
             base_path: str = "/data/driver_drowsiness/dataset.mat"
@@ -117,17 +161,49 @@ class Drowsiness(Dataset):
 
     def __getitem__(self, item):
         x = torch.tensor(self.x[item], dtype=torch.float)
-        y = torch.tensor(self.y[item], dtype=torch.float)
+        y = torch.tensor(self.y[item], dtype=torch.long)
 
         return x, y
 
 
-if __name__ == '__main__':
-    driver = Drowsiness()
-    dataloader = DataLoader(dataset=driver, batch_size=64, shuffle=True)
+class MotorImageryDataset(Dataset):
+    def __init__(self,
+                 data_path='/data/openbmi_mi'
+                 ):
+        total_x, total_y = [], []
+        for path in os.listdir(data_path)[:4]:
+            if os.path.basename(path).split('_')[0] == 'sess01':
+                path = os.path.join(data_path, path)
+                data = np.load(path)
+                total_x.append(data['x'])
+                total_y.append(data['y'])
+        self.x = np.concatenate(total_x)
+        self.y = np.concatenate(total_y)
+        self.ch_names = ['Fp1', 'Fp2', 'F7', 'F3', 'Fz', 'F4', 'F8', 'FC5', 'FC1', 'FC2', 'FC6', 'T7', 'C3', 'Cz', 'C4',
+                    'T8', 'TP9', 'CP5', 'CP1', 'CP2', 'CP6', 'TP10',
+                    'P7', 'P3', 'Pz', 'P4', 'P8', 'PO9', 'O1', 'Oz', 'O2', 'PO10', 'FC3', 'FC4', 'C5', 'C1', 'C2', 'C6',
+                    'CP3', 'CPz', 'CP4', 'P1', 'P2', 'POz', 'FT9', 'FTT9h',
+                    'TTP7h', 'TP7', 'TPP9h', 'FT10', 'FTT10h', 'TPP8h', 'TP8', 'TPP10h', 'F9', 'F10', 'AF7', 'AF3',
+                    'AF4', 'AF8', 'PO3', 'PO4']
+        self.sfreq = 250
+        self.event_name = ['left', 'right']  # {'left': 0, 'right': 1}
 
-    for x, y in dataloader:
+    def __getitem__(self, item):
+        x = torch.tensor(self.x[item], dtype=torch.float)
+        y = torch.tensor(self.y[item], dtype=torch.long)
+        return x, y
+
+    def __len__(self):
+        return len(self.y)
+
+
+if __name__ == '__main__':
+    dataset = MotorImageryDataset()
+    for data in dataset:
+        x, y = data
         print(x.shape, y.shape)
+        print(y)
+
 
 
 
